544c4919bd37e8307cab08f2a7a5e23f33bce9c8
David Arnold <d@0x1.org>
2017-04-06 13:26:27 +0000

    Basic structure.

---
+++ fixtool/session.py
@@ -0,0 +1,53 @@
+
+class Session(object):
+    pass
+
+class ClientSession(Session):
+
+    def __init__(self):
+        """Constructor."""
+        self.host = ''
+        self.port = 0
+        self.socket = None
+        self.sender_comp_id = None
+        self.target_comp_id = None
+        self.username = ''
+        self.password = ''
+        self.do_auto_heartbeat = True
+        self.history = None
+        self.message_validator = None
+        self.workflow_validator = None
+        return
+
+    def connect(self):
+        """Connect to peer socket."""
+        return
+
+    def disconnect(self):
+        """Disconnect from peer socket."""
+        return
+
+    def send(self):
+        """Send a message to the peer entity."""
+        return
+
+    def received_count(self):
+        """Get the number of unread messages received."""
+        return 0
+
+    def recv(self):
+        """Get a received message."""
+        return
+
+
+
+class ServerSession(Session):
+    pass
+
+class AcceptedSession(Session):
+    pass
+
+
+class SessionHistory(object):
+    pass
+
