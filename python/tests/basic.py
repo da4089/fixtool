553846f101448242feae49205ad05ef4ee290887
David Arnold <d@0x1.org>
2018-11-11 00:29:14 +1100

    Convert tests to use pytest.

--- python/tests/basic.py
+++ python/tests/basic.py
@@ -25,74 +25,68 @@
 
 import fixtool
 import simplefix
-import unittest
 
 
-class BasicTests(unittest.TestCase):
+def test_spawn():
+    proxy = fixtool.spawn_agent()
+    assert proxy is not None
 
-    def setUp(self):
-        return
+    proxy.shutdown()
+    return
 
-    def tearDown(self):
-        return
 
-    def test_spawn(self):
-        proxy = fixtool.spawn_agent()
-        self.assertIsNotNone(proxy)
+def test_create_server():
+    proxy = fixtool.spawn_agent()
+    assert proxy is not None
 
-        proxy.shutdown()
-        return
+    s1 = proxy.create_server("s1")
+    s1.listen(23456)
 
-    def test_create_server(self):
-        proxy = fixtool.spawn_agent()
-        self.assertIsNotNone(proxy)
+    c1 = proxy.create_client("c1")
+    c1.connect('localhost', 23456)
 
-        s1 = proxy.create_server("s1")
-        s1.listen(23456)
+    assert s1.pending_accept_count() == 1
+    cs1 = s1.accept("cs1")
+    assert c1.is_connected()
+    assert cs1.is_connected()
 
-        c1 = proxy.create_client("c1")
-        c1.connect('localhost', 23456)
+    fix_msg = simplefix.FixMessage()
+    fix_msg.append_pair(8, "FIX.4.2")
+    fix_msg.append_pair(35, 0)
+    fix_msg.append_pair(34, 1)
+    fix_msg.append_utc_timestamp(52)
+    send_buf = fix_msg.encode()
 
-        self.assertEqual(1, s1.pending_accept_count())
-        cs1 = s1.accept("cs1")
-        self.assertTrue(c1.is_connected())
-        self.assertTrue(cs1.is_connected())
+    c1.send(send_buf)
+    assert cs1.receive_queue_length() == 1
 
-        fix_msg = simplefix.FixMessage()
-        fix_msg.append_pair(8, "FIX.4.2")
-        fix_msg.append_pair(35, 0)
-        fix_msg.append_pair(34, 1)
-        fix_msg.append_utc_timestamp(52)
-        send_buf = fix_msg.encode()
+    #m1 = cs1.receive()
+    #self.assertEqual("hello", m1)
 
-        c1.send(send_buf)
-        self.assertEqual(1, cs1.receive_queue_length())
+    c1.destroy()
+    s1.destroy()
+    proxy.shutdown()
+    return
 
-        #m1 = cs1.receive()
-        #self.assertEqual("hello", m1)
+def xxx_test_connect_disconnect():
+    proxy = fixtool.FixToolProxy("localhost", 11011)
+    client = proxy.create_client("c1")
+    server = proxy.create_server("s1")
+    server.listen(12000)
+    assert server.pending_accept_count() == 0
 
-        c1.destroy()
-        s1.destroy()
-        proxy.shutdown()
-        return
+    client.connect("localhost", 12000)
+    assert server.pending_accept_count() == 1
 
-    def xxx_test_connect_disconnect(self):
-        proxy = fixtool.FixToolProxy("localhost", 11011)
-        client = proxy.create_client("c1")
-        server = proxy.create_server("s1")
-        server.listen(12000)
-        self.assertEqual(0, server.pending_accept_count())
-        client.connect("localhost", 12000)
-        self.assertEqual(1, server.pending_accept_count())
-        server_session = server.accept("ss1")
-        self.assertTrue(server_session.is_connected())
-        self.assertTrue(client.is_connected())
+    server_session = server.accept("ss1")
+    assert server_session.is_connected()
+    assert client.is_connected()
 
-        server_session.disconnect()
-        self.assertFalse(server_session.is_connected())
-        self.assertFalse(client.is_connected())
+    server_session.disconnect()
+    assert not server_session.is_connected()
+    assert not client.is_connected()
 
-        client.destroy()
-        server_session.destroy()
-        server.destroy()
-        return
+    client.destroy()
+    server_session.destroy()
+    server.destroy()
+    return
